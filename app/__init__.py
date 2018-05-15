8fc15f54c3153797f9f85fa588898c23b64e3bb3
evezus <keepik3@gmail.com>
2018-05-15 21:30:55 +0300

    add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
+CORS(app)
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
